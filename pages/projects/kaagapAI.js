925212147b0c0f63fc5e5292cc6044861f636c02
Julian Aubrey Alonzo <julianaubreya@gmail.com>
2019-12-17 23:17:32 -0700

    Added screenshots in kaagapai project

--- pages/projects/kaagapAI.js
+++ pages/projects/kaagapAI.js
@@ -41,9 +41,9 @@ export default function KaagapAI() {
         coverPhotoPath="/static/kaagapai/kaagapai-bg.svg"
         mainProjectImagePath="/static/kaagapai/documentAnalysis.gif"
       />
-      <div className="container mx-auto mt-12 mb-64 w-11/12 sm:4/5 md:w-3/4 lg:w-3/5 xl:w-1/2">
+      <div className="container mx-auto my-12 w-11/12 sm:4/5 md:w-3/4 lg:w-3/5 xl:w-1/2">
         <ProjectTitle
-          logoPath="/static/kaagapai-logo.svg"
+          logoPath="/static/kaagapai/kaagapai-logo.svg"
           title="kaagapAI"
           websiteUrl="https://kaagapai.herokuapp.com"
           githubUrl="https://github.com/prometheus-tech/kaagapAI"
@@ -67,11 +67,16 @@ export default function KaagapAI() {
           </h2>
           <Chips chips={apis} />
         </div>
-        <div className="mb-10">
+        <div>
           <h2 className="text-2xl font-sans font-bold mb-4">Features</h2>
-          <h3 className="text-lg font-sans font-bold mb-1">
+          <h3 className="text-lg font-sans font-bold mb-4">
             Document Analysis
           </h3>
+          <img
+            className="border border-gray-300 rounded-lg mb-4"
+            src="/static/kaagapai/documentAnalysis.gif"
+            alt="Document Analysis Demo"
+          />
           <p className="font-sans leading-loose mb-8">
             kaagapAI uses IBM Watson Natural Language Understanding in order to
             perform text analysis on session documents (e.g., transcripts and
@@ -81,7 +86,7 @@ export default function KaagapAI() {
             session or multiple sessions. Analysis on multiple sessions include
             data visualizations on sentiment and emotion trends.
           </p>
-          <p className="font-sans leading-loose mb-8">
+          <p className="font-sans leading-loose mb-8 mb-8">
             IBM Watson Knowledge Studio enables the training of custom models in
             order to extract domain-specific entities. The original intention
             was to train a custom model to extract more relevant entities in the
@@ -94,40 +99,70 @@ export default function KaagapAI() {
           <h4 className="font-sans text-gray-700 mb-1">
             Word Cloud (Keywords)
           </h4>
-          <p className="font-sans leading-loose mb-8">
+          <p className="font-sans leading-loose">
             The word cloud consists of the keywords that occurred multiple times
             in the document. Clicking on a specific word in the word cloud
             displays the word count and relevance and finds all search
             apperances of the word across analyzed documents.
           </p>
+          <img
+            className="border border-gray-300 rounded-lg mt-4 mb-8"
+            src="/static/kaagapai/keywords.png"
+            alt="Keywords"
+          />
           <h4 className="font-sans text-gray-700 mb-1">Categories</h4>
-          <p className="font-sans leading-loose mb-8">
+          <p className="font-sans leading-loose">
             <i>Categories</i> shows the hierarchy of themes that were detected
             in the analyzed documents. These themes were derived from the words
             that were found in the documents.
           </p>
+          <img
+            className="border border-gray-300 rounded-lg mt-4 mb-8"
+            src="/static/kaagapai/categories.png"
+            alt="Categories"
+          />
           <h4 className="font-sans text-gray-700 mb-1">Entities</h4>
-          <p className="font-sans leading-loose mb-8">
+          <p className="font-sans leading-loose">
             <i>Entities</i> classifies words that belong to pre-defined
             categories. Clicking on a specific entity shows the entity type,
             relevance, and appearances across analyzed documents.
           </p>
+          <img
+            className="border border-gray-300 rounded-lg mt-4 mb-8"
+            src="/static/kaagapai/entities.png"
+            alt="Entities"
+          />
           <h4 className="font-sans text-gray-700 mb-1">Emotions</h4>
-          <p className="font-sans leading-loose mb-8">
+          <p className="font-sans leading-loose">
             <i>Emotions</i> rates the detected emotions of analyzed documents
             according to six basic emotions: joy, anger, disgust, sadness, and
             fear. In the analysis of multiple sessions, a line graph is used to
             show the trend of emotions across selected sessions.
           </p>
+          <img
+            className="border border-gray-300 rounded-lg mt-4 mb-8"
+            src="/static/kaagapai/emotions.png"
+            alt="Emotions"
+          />
           <h4 className="font-sans text-gray-700 mb-1">Sentiment</h4>
-          <p className="font-sans leading-loose mb-8">
+          <p className="font-sans leading-loose">
             <i>Sentiment</i> scores the overall feel of the session which could
             be positive, negative, or neutral. Analysis of multiple sessions
             also shows the trend of sentiments across selected sessions.
           </p>
+          <img
+            className="border border-gray-300 rounded-lg mt-4 mb-8"
+            src="/static/kaagapai/sentiment.png"
+            alt="Sentiment"
+          />
           <h3 className="text-lg font-sans font-bold mb-1">
             Document Preprocessing
           </h3>
+          <img
+            className="border border-gray-300 rounded-lg mt-4 mb-8"
+            src="/static/kaagapai/systemArchitecture.png"
+            alt="System Architecture"
+          />
           <p className="font-sans leading-loose mb-8">
             kaagapAI preprocesses uploaded documents in order to extract text
             from different file formats (i.e., txt, pdf, docx, png, jpg, m4a).
@@ -137,10 +172,15 @@ export default function KaagapAI() {
             uploaded files, the text is translated to English using the Cloud
             Translation API.
           </p>
+          <img
+            className="border border-gray-300 rounded-lg mb-8"
+            src="/static/kaagapai/documentPreprocessing.gif"
+            alt="Document Preprocessing"
+          />
           <h3 className="text-lg font-sans font-bold mb-1">
             Client, Session, and Document Management
           </h3>
-          <p className="font-sans leading-loose mb-8">
+          <p className="font-sans leading-loose">
             kaagapAI organizes its data based on the common data organization of
             mental health practitioners. A client can have multiple sessions,
             and a session can hold multiple documents. kaagapAI provides CRUD
@@ -154,6 +194,11 @@ export default function KaagapAI() {
             are not excluded in the analysis. Documents can be set to ignored to
             exclude them from the analysis.
           </p>
+          <img
+            className="border border-gray-300 rounded-lg mt-4"
+            src="/static/kaagapai/entityManagement.gif"
+            alt="Entity Management"
+          />
         </div>
       </div>
     </div>
