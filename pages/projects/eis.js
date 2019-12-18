7b6e276729693880a740fcdade6a1fbf2b17f668
Julian Aubrey Alonzo <julianaubreya@gmail.com>
2019-12-18 01:12:14 -0700

    Increased margin spacing between features in eis

--- pages/projects/eis.js
+++ pages/projects/eis.js
@@ -65,69 +65,79 @@ export default function EIS() {
         </div>
         <div>
           <h2 className="text-2xl font-sans font-bold mb-4">Features</h2>
-          <h3 className="text-lg font-sans font-bold mb-2">Item Management</h3>
-          <p className="font-sans leading-loose mb-8">
-            EIS enables users to define and add items into their system. Item
-            name, category, condition, and thumbnails are the default fields for
-            an item, however, users may add custom fields of an item in the{' '}
-            <i>Properties</i> section.
-          </p>
-          <img
-            className="border border-gray-300 rounded-lg mb-8"
-            src="/static/eis/itemManagement.gif"
-            alt="Item Management Demo"
-          />
-          <p className="font-sans leading-loose mb-8">
-            Properties are custom fields that describe attributes of an item.
-            For example, a laptop may have properties such as processor, storage
-            capacity, and price. Users can define any property they want in
-            order to represent any item.
-          </p>
-          <p className="font-sans leading-loose mb-8">
-            EIS allows users to add supporting information of an item through{' '}
-            <i>notes</i> and <i>attachments</i>. Notes may be any important
-            message such as a message informing the user how an item was broken
-            or who borrowed the equipment. Attachments are relevant files
-            associated to an item. These files may be software license,
-            receipts, and images.
-          </p>
-          <h3 className="text-lg font-sans font-bold mt-4 mb-2">
-            Folder Organization
-          </h3>
-          <p className="font-sans leading-loose mb-8">
-            EIS organizes items into folders in order to keep related records
-            together. Nesting folders is possible in order to support different
-            ways to organize items. EIS also enables users to move items and
-            folders into another folder in case an item was moved or a different
-            folder organization is adopted.
-          </p>
-          <img
-            className="border border-gray-300 rounded-lg mb-8"
-            src="/static/eis/folderOrganization.gif"
-            alt="Folder Organization Demo"
-          />
-          <h3 className="text-lg font-sans font-bold mt-4 mb-2">Templates</h3>
-          <p className="font-sans leading-loose mb-8">
-            EIS provides users a way to create item templates for items that
-            have standard fields (e.g., properties, attachments, and notes).
-            These templates may be used to pre-populate fields when creating new
-            items into different folders.
-          </p>
-          <img
-            className="border border-gray-300 rounded-lg mb-8"
-            src="/static/eis/templates.gif"
-            alt="Templates Demo"
-          />
-          <h3 className="text-lg font-sans font-bold mt-2 mb-2">Search</h3>
-          <p className="font-sans leading-loose">
-            EIS is capable of searching for items by id, name, category,
-            condition, properties, and notes.
-          </p>
-          <img
-            className="border border-gray-300 rounded-lg mt-4 mb-8"
-            src="/static/eis/search.gif"
-            alt="Search Demo"
-          />
+          <div className="mb-12">
+            <h3 className="text-lg font-sans font-bold mb-2">
+              Item Management
+            </h3>
+            <p className="font-sans leading-loose mb-8">
+              EIS enables users to define and add items into their system. Item
+              name, category, condition, and thumbnails are the default fields
+              for an item, however, users may add custom fields of an item in
+              the <i>Properties</i> section.
+            </p>
+            <img
+              className="border border-gray-300 rounded-lg mb-8"
+              src="/static/eis/itemManagement.gif"
+              alt="Item Management Demo"
+            />
+            <p className="font-sans leading-loose mb-8">
+              Properties are custom fields that describe attributes of an item.
+              For example, a laptop may have properties such as processor,
+              storage capacity, and price. Users can define any property they
+              want in order to represent any item.
+            </p>
+            <p className="font-sans leading-loose">
+              EIS allows users to add supporting information of an item through{' '}
+              <i>notes</i> and <i>attachments</i>. Notes may be any important
+              message such as a message informing the user how an item was
+              broken or who borrowed the equipment. Attachments are relevant
+              files associated to an item. These files may be software license,
+              receipts, and images.
+            </p>
+          </div>
+          <div className="mb-12">
+            <h3 className="text-lg font-sans font-bold mb-2">
+              Folder Organization
+            </h3>
+            <p className="font-sans leading-loose mb-8">
+              EIS organizes items into folders in order to keep related records
+              together. Nesting folders is possible in order to support
+              different ways to organize items. EIS also enables users to move
+              items and folders into another folder in case an item was moved or
+              a different folder organization is adopted.
+            </p>
+            <img
+              className="border border-gray-300 rounded-lg"
+              src="/static/eis/folderOrganization.gif"
+              alt="Folder Organization Demo"
+            />
+          </div>
+          <div className="mb-12">
+            <h3 className="text-lg font-sans font-bold mb-2">Templates</h3>
+            <p className="font-sans leading-loose mb-8">
+              EIS provides users a way to create item templates for items that
+              have standard fields (e.g., properties, attachments, and notes).
+              These templates may be used to pre-populate fields when creating
+              new items into different folders.
+            </p>
+            <img
+              className="border border-gray-300 rounded-lg"
+              src="/static/eis/templates.gif"
+              alt="Templates Demo"
+            />
+          </div>
+          <div className="mb-12">
+            <h3 className="text-lg font-sans font-bold mb-2">Search</h3>
+            <p className="font-sans leading-loose mb-8">
+              EIS is capable of searching for items by id, name, category,
+              condition, properties, and notes.
+            </p>
+            <img
+              className="border border-gray-300 rounded-lg mb-8"
+              src="/static/eis/search.gif"
+              alt="Search Demo"
+            />
+          </div>
         </div>
       </div>
     </div>
