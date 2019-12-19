8099a337df8a18d1b70b9080a0b889f80851cb65
Julian Aubrey Alonzo <julianaubreya@gmail.com>
2019-12-18 22:39:05 -0700

    Added key in chips

--- components/Chips.js
+++ components/Chips.js
@@ -2,7 +2,10 @@ export default function Chips({ chips = [] }) {
   return (
     <ul className="flex flex-wrap">
       {chips.map(chip => (
-        <li className="mr-1 mb-3 text-xs bg-gray-300 text-gray-800 rounded-full px-4 py-1">
+        <li
+          key={chip}
+          className="mr-1 mb-3 text-xs bg-gray-300 text-gray-800 rounded-full px-4 py-1"
+        >
           {chip}
         </li>
       ))}
